a259e85cd5bf632a0f4aab22a0f4c4630e2c4f85
Liseth Giraldo <liseth.giraldo@rocketfy.co>
2023-05-26 19:54:28 -0500

    feat: se inicia ejercio 4

--- js/ejercicio19.js
+++ js/ejercicio19.js
@@ -1,1 +1,63 @@
-console.log("Ejercicio 19");
+const readline = require('readline');
+
+function calcularCodigos() {
+  const nombres = [];
+  const apellidosPaterno = [];
+  const apellidosMaterno = [];
+  const notas1 = [];
+  const notas2 = [];
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  rl.question('Ingrese los datos de los alumnos:\n', function (input) {
+    const alumnos = input.split('\n');
+
+    for (let i = 0; i < alumnos.length; i++) {
+      const alumno = alumnos[i].split(',');
+
+      nombres.push(alumno[0].trim());
+      apellidosPaterno.push(alumno[1].trim());
+      apellidosMaterno.push(alumno[2].trim());
+      notas1.push(parseFloat(alumno[3].trim()));
+      notas2.push(parseFloat(alumno[4].trim()));
+    }
+
+    const codigos = [];
+    const promedios = [];
+    let aprobados = 0;
+    let desaprobados = 0;
+
+    for (let i = 0; i < nombres.length; i++) {
+      const codigo = apellidosPaterno[i].substring(0, 2) +
+        apellidosMaterno[i].substring(1, 3) +
+        nombres[i].substring(nombres[i].length - 2) +
+        (i + 1);
+
+      codigos.push(codigo);
+
+      const promedio = (notas1[i] + notas2[i]) / 2;
+      promedios.push(promedio);
+
+      if (promedio >= 10.5) {
+        aprobados++;
+      } else {
+        desaprobados++;
+      }
+    }
+
+    console.log('Códigos y promedios de los alumnos:');
+    for (let i = 0; i < codigos.length; i++) {
+      console.log(codigos[i] + ' - ' + promedios[i]);
+    }
+
+    console.log('Cantidad de alumnos aprobados: ' + aprobados);
+    console.log('Cantidad de alumnos desaprobados: ' + desaprobados);
+
+    rl.close();
+  });
+}
+
+calcularCodigos();
--- js/ejercicio4.js
+++ js/ejercicio4.js
@@ -1,1 +1,29 @@
-console.log("Ejercicio 4");
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let N = 0;
+let Suma = 0;
+
+rl.question('Ingrese un número: ', function (numero) {
+    N = 0;
+    Suma = 0;
+
+    function calcularSumatoria() {
+        N = N + 1;
+        Suma = Suma + N;
+
+        if (N === numero) {
+            console.log('La sumatoria de los números del 1 al ' + numero + ' es: ' + Suma);
+            rl.close();
+        } else {
+            N = N + 1;
+            Suma = Suma + N;
+        }
+    }
+
+    calcularSumatoria();
+});
